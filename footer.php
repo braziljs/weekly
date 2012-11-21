27e8570f013a9bd67fcf844131544c2822ee950f
Keppelen <keppelen.as@gmail.com>
2012-11-21 00:46:29 -0200

    remove .php - add staticos

--- footer.php
+++ footer.php
@@ -15,55 +15,55 @@
             Participe do nosso <a href="https://groups.google.com/forum/?fromgroups#!forum/braziljs-foundation" style="color:#4085CA;">grupo de discussão</a>
           </p>
           <style type="text/css">
-#mc_embed_signup input[type="text"], input[type="email"] {
-  width: 400px;
-  float: left;
-  font-size: 20px;
-  margin: 25px 38px 0 0;
-}
+            #mc_embed_signup input[type="text"], input[type="email"] {
+              width: 400px;
+              float: left;
+              font-size: 20px;
+              margin: 25px 38px 0 0;
+            }
 
-input[type="text"], input[type="email"] {
-  display: block;
-  color: #6C6D70;
-  border: 1px solid #D1D2D3;
-  background: white;
-  border-radius: 6px;
-  -moz-box-shadow: inset 2px 2px 11px 0 #dbddde;
-  -webkit-box-shadow: inset 2px 2px 11px 0 #DBDDDE;
-  box-shadow: inset 2px 2px 11px 0 #DBDDDE;
-  padding: 8px 10px;
-  height: 35px;
-}
+            input[type="text"], input[type="email"] {
+              display: block;
+              color: #6C6D70;
+              border: 1px solid #D1D2D3;
+              background: white;
+              border-radius: 6px;
+              -moz-box-shadow: inset 2px 2px 11px 0 #dbddde;
+              -webkit-box-shadow: inset 2px 2px 11px 0 #DBDDDE;
+              box-shadow: inset 2px 2px 11px 0 #DBDDDE;
+              padding: 8px 10px;
+              height: 35px;
+            }
 
-input[type="submit"] {
-  margin-top: 25px;
-float: right;
-margin-bottom: 35px;
-}
+            input[type="submit"] {
+              margin-top: 25px;
+            float: right;
+            margin-bottom: 35px;
+            }
 
-input[type="submit"] {
-  font: bold 20px/.9em 'Helvetica', sans-serif;
-  display: block;
-  height: 50px;
-  color: #8F5A0A;
-  cursor: pointer;
-  border-width: 1px;
-  border-style: solid;
-  border-color: #D69E31 #E3A037 #D5982D #E3A037;
-  background-color: #FFB94B;
-  background-image: -webkit-gradient(linear, left top, left bottom, from(#FDDB6F), to(#FFB94B));
-  background-image: -webkit-linear-gradient(top, #FDDB6F, #FFB94B);
-  background-image: -moz-linear-gradient(top, #FDDB6F, #FFB94B);
-  background-image: -ms-linear-gradient(top, #FDDB6F, #FFB94B);
-  background-image: -o-linear-gradient(top, #FDDB6F, #FFB94B);
-  background-image: linear-gradient(top, #FDDB6F, #FFB94B);
-  border-radius: 6px;
-  text-shadow: 0 1px 0 rgba(255, 255, 255, 0.5);
-  -moz-box-shadow: 0 0 1px rgba(0, 0, 0, 0.3), 0 1px 0 rgba(255, 255, 255, 0.3) inset;
-  -webkit-box-shadow: 0 0 1px rgba(0, 0, 0, 0.3), 0 1px 0 rgba(255, 255, 255, 0.3) inset;
-  box-shadow: 0 0 1px rgba(0, 0, 0, 0.3), 0 1px 0 rgba(255, 255, 255, 0.3) inset;
-}
-</style>
+            input[type="submit"] {
+              font: bold 20px/.9em 'Helvetica', sans-serif;
+              display: block;
+              height: 50px;
+              color: #8F5A0A;
+              cursor: pointer;
+              border-width: 1px;
+              border-style: solid;
+              border-color: #D69E31 #E3A037 #D5982D #E3A037;
+              background-color: #FFB94B;
+              background-image: -webkit-gradient(linear, left top, left bottom, from(#FDDB6F), to(#FFB94B));
+              background-image: -webkit-linear-gradient(top, #FDDB6F, #FFB94B);
+              background-image: -moz-linear-gradient(top, #FDDB6F, #FFB94B);
+              background-image: -ms-linear-gradient(top, #FDDB6F, #FFB94B);
+              background-image: -o-linear-gradient(top, #FDDB6F, #FFB94B);
+              background-image: linear-gradient(top, #FDDB6F, #FFB94B);
+              border-radius: 6px;
+              text-shadow: 0 1px 0 rgba(255, 255, 255, 0.5);
+              -moz-box-shadow: 0 0 1px rgba(0, 0, 0, 0.3), 0 1px 0 rgba(255, 255, 255, 0.3) inset;
+              -webkit-box-shadow: 0 0 1px rgba(0, 0, 0, 0.3), 0 1px 0 rgba(255, 255, 255, 0.3) inset;
+              box-shadow: 0 0 1px rgba(0, 0, 0, 0.3), 0 1px 0 rgba(255, 255, 255, 0.3) inset;
+            }
+        </style>
 <div id="mc_embed_signup">
   <form action="http://braziljs.us5.list-manage1.com/subscribe/post?u=77b6594f10bba05dcc722c80e&amp;id=e6beed4270" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate="">
     <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="seu@email.com" required="">
